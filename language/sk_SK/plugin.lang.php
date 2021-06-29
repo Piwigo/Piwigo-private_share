f259ec167e93a128725a30e7dabdf3d282abe957
Jose Riha <jose1711@gmail.com>
2021-06-29 14:04:19 +0200

    Add Slovak translation

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+$lang['Invalid email address'] = 'Neplatná e-mailová adresa';
+$lang['Close'] = 'Zavrieť';
+$lang['Share'] = 'Zdieľať';
+$lang['Expires in'] = 'Vyprší o';
+$lang['Send email'] = 'Poslať e-mail';
+
+
+$lang['Apply to albums'] = 'Použiť na albumy';
+$lang['all albums'] = 'všetky albumy';
+$lang['a list of albums'] = 'zoznam albumov';
+?>
