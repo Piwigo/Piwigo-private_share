289403d2f4e4d6fc5a9f9cf6333e2e88a5e7c94d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-14 12:54:57 +0100

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Bruk i album';
+$lang['Close'] = 'Lukk';
+$lang['Expires in'] = 'Utgår om';
+$lang['Invalid email address'] = 'Ugyldig epostadresse';
+$lang['Send email'] = 'Send e-post';
+$lang['Share'] = 'Del';
+$lang['a list of albums'] = 'en liste over album';
+$lang['all albums'] = 'alle album';
