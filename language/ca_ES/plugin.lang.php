2da3f8e0663a05fceb7bfb46528c5e50fbc6ad40
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-14 16:44:42 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Aplica als àlbums';
+$lang['Close'] = 'Tanca';
+$lang['Expires in'] = 'Caduca en';
+$lang['Invalid email address'] = 'Adreça de correu electrònic no vàlida';
+$lang['Send email'] = 'Envia missatge';
+$lang['Share'] = 'Comparteix';
+$lang['a list of albums'] = 'una llista d\'àlbums';
+$lang['all albums'] = 'tots els àlbums';
