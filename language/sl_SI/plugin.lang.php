5e96ed411761803100c8cc0d8efc9a7f57ee092a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-14 11:01:41 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Uporabi za albume';
+$lang['Close'] = 'Zapri';
+$lang['Expires in'] = 'Poteče čez';
+$lang['Invalid email address'] = 'Nepravilen e-mail naslov';
+$lang['Send email'] = 'Pošlji e-pošto';
+$lang['Share'] = 'Delite';
+$lang['a list of albums'] = 'seznam albumov';
+$lang['all albums'] = 'vsi albumi';
