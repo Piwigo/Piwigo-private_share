df38c6bde96b9a9d54e22a6912a67e72cb9575a5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-14 11:01:31 +0100

    Update ro_RO, thanks to Jerome-PH

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Aplicați la albume';
+$lang['Close'] = 'Închide';
+$lang['Expires in'] = 'Expira in';
+$lang['Invalid email address'] = 'Adresa de email invalida';
+$lang['Send email'] = 'Trimite email';
+$lang['Share'] = 'Distribuie';
+$lang['a list of albums'] = 'o listă de albume';
+$lang['all albums'] = 'toate albumele';
