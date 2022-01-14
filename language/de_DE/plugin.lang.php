1bbf0972293533a673e9143a0d6410cebf43d688
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-14 16:44:47 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Share'] = 'Teilen Sie';
+$lang['a list of albums'] = 'eine Liste von Alben';
+$lang['all albums'] = 'alle Alben';
+$lang['Invalid email address'] = 'Ungültige E-Mail Adresse';
+$lang['Send email'] = 'E-Mail senden';
+$lang['Apply to albums'] = 'Auf Alben anwenden';
+$lang['Close'] = 'Schließen';
+$lang['Expires in'] = 'Läuft ab in';
