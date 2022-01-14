61c01524aaea684cf5a2408650d22ea84a66843f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-14 11:01:36 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = 'alle albummer';
+$lang['Expires in'] = 'Udløber om';
+$lang['Apply to albums'] = 'Udfør på albummer';
+$lang['Close'] = 'Luk';
+$lang['Invalid email address'] = 'Ugyldig mailadresse';
+$lang['Send email'] = 'Send mail';
+$lang['Share'] = 'Del';
+$lang['a list of albums'] = 'en liste over albummer';
