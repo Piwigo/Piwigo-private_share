65ee7cc87577cc1ea068dbe7fc3e4102c37b9ee8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-14 21:05:05 +0100

    Update et_EE, thanks to deemon

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Expires in'] = 'Aegub';
+$lang['Close'] = 'Sulge';
+$lang['Apply to albums'] = 'Rakenda albumitele';
+$lang['Invalid email address'] = 'Ebakorrektne e-posti aadress';
+$lang['all albums'] = 'kõik albumid';
+$lang['a list of albums'] = 'albumite nimekiri';
+$lang['Share'] = 'Jaga';
+$lang['Send email'] = 'Saada e-post';
