4e854d6417e065214380504e2917ab4698d46640
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-15 11:15:27 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Aplicar a los álbumes';
+$lang['Close'] = 'Cerrar';
+$lang['Expires in'] = 'Caduca en';
+$lang['Invalid email address'] = 'Dirección de correo no válida';
+$lang['Send email'] = 'Enviar mensaje';
+$lang['Share'] = 'Compartir';
+$lang['a list of albums'] = 'una lista de álbumes';
+$lang['all albums'] = 'todos los álbumes';
