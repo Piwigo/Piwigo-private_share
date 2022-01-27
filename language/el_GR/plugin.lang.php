cdb3049d7162b59d4656e331df5cc7a099e0bb56
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-27 05:43:34 +0100

    Update el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['a list of albums'] = 'Λίστα λευκωμάτων';
+$lang['all albums'] = 'όλα τα λευκώματα';
+$lang['Share'] = 'Κοινοποίηση';
+$lang['Send email'] = 'Αποστολή email';
+$lang['Invalid email address'] = 'Μη έγκυρη διεύθυνση e-mail';
+$lang['Expires in'] = 'Λήγει σε';
+$lang['Apply to albums'] = 'Εφαρμογή σε λευκώματα';
+$lang['Close'] = 'Τέλος';
