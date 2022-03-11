e5374624f7713c2d6a6a8153c452892d768010af
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-03-11 05:59:04 +0100

    Update sk_SK, thanks to jose1711

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Použiť na albumy';
+$lang['Close'] = 'Zavrieť';
+$lang['Expires in'] = 'Expiruje o';
+$lang['Invalid email address'] = 'Neplatná e-mailová adresa';
+$lang['Send email'] = 'Odoslať e-mail';
+$lang['Share'] = 'Zdieľať';
+$lang['a list of albums'] = 'zoznam albumov';
+$lang['all albums'] = 'všetky albumy';
