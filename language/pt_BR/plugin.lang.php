c80dd9ae048c5f61b8461134fecd70a3525d7c2a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-03 18:35:46 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Aplicar a álbuns';
+$lang['Close'] = 'Fechar';
+$lang['Expires in'] = 'Expira em';
+$lang['Invalid email address'] = 'Endereço de e-mail inválido';
+$lang['Send email'] = 'Enviar e-mail';
+$lang['Share'] = 'Compartilhar';
+$lang['a list of albums'] = 'uma lista dos álbuns';
+$lang['all albums'] = 'todos os albúns';
