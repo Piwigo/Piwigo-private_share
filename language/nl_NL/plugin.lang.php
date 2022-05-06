b56e81889c7b79de079db4eda2863a67f7be0cf2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-06 06:16:17 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = 'Alle albums';
+$lang['a list of albums'] = 'Een lijst van albums';
+$lang['Share'] = 'Deel';
+$lang['Send email'] = 'Mail van de zender';
+$lang['Invalid email address'] = 'Geen geldig email-adres';
+$lang['Expires in'] = 'Verloopt in';
+$lang['Close'] = 'Sluit';
+$lang['Apply to albums'] = 'Toepassen op albums';
