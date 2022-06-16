0f83a5fcbb3a4740c6b601ce26eabf1b4712b728
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-16 06:22:43 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Virkja á albúmum';
+$lang['Close'] = 'Loka';
+$lang['Expires in'] = 'Rennur út eftir';
+$lang['Invalid email address'] = 'Ógilt netfang';
+$lang['Send email'] = 'Senda tölvupóst';
+$lang['Share'] = 'Deila';
+$lang['a list of albums'] = 'listi yfir albúm';
+$lang['all albums'] = 'öll albúm';
