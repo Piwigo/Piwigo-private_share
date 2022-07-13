4600b493e264ef0adf686519a6f38035ddb21ccd
HWFord <lilypickle@hotmail.fr>
2022-07-13 09:37:06 +0200

    Add missing EN and FR translations

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -4,8 +4,14 @@ $lang['Close'] = 'Fermer';
 $lang['Share'] = 'Partager';
 $lang['Expires in'] = 'Expire dans';
 $lang['Send email'] = 'Envoyer l\'email';
-
 $lang['Apply to albums'] = 'Appliquer aux albums';
 $lang['all albums'] = 'tous les albums';
 $lang['a list of albums'] = 'une liste d\'albums';
+$lang['Photo shared'] = 'Photo partagé';
+$lang['Your email has been sent'] = 'Votre email à été envoyé';
+$lang['Your email could not be sent'] = 'Votre email n\'a pas pu être envoyé';
+$lang['Share this picture'] = 'Partager cette photo';
+$lang['Click here to see the picture'] = 'Cliquez ici pour voir la photo';
+$lang['Share link'] = 'Lien de partage';
+$lang['%s has shared a photo with you'] = '%s a partagé une photo avec vous';
 ?>
