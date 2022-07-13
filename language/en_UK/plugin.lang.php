07f456d7929e936a90a31042143173e68de889fb
HWFord <lilypickle@hotmail.fr>
2022-07-13 09:37:06 +0200

    Add missing EN and FR translations

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -4,9 +4,15 @@ $lang['Close'] = 'Close';
 $lang['Share'] = 'Share';
 $lang['Expires in'] = 'Expires in';
 $lang['Send email'] = 'Send email';
-
-
 $lang['Apply to albums'] = 'Apply to albums';
 $lang['all albums'] = 'all albums';
 $lang['a list of albums'] = 'a list of albums';
+$lang['Photo shared'] = 'Photo shared';
+$lang['Your email has been sent'] = 'Your email has been sent';
+$lang['Your email could not be sent'] = 'Your email could not be sent';
+$lang['Share this picture'] = 'Share this picture';
+$lang['Click here to see the picture'] = 'Click here to see the picture';
+$lang['Share link'] = 'Share link';
+$lang['%s has shared a photo with you'] = '%s has shared a photo with you';
+
 ?>
