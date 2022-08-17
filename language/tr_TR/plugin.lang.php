ba80a1578cb36949daf710a6d70c7cb82ff0f42d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-08-17 13:45:24 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Albümlere uygula';
+$lang['Close'] = 'Kapat';
+$lang['Expires in'] = 'Sona ermesi';
+$lang['Invalid email address'] = 'Geçersiz e-posta adresi';
+$lang['Send email'] = 'E-posta gönder';
+$lang['Share'] = 'Paylaş';
+$lang['a list of albums'] = 'bir albüm listesi';
+$lang['all albums'] = 'bütün albüm';
