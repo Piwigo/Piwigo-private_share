add152f86dd8e618691ae8fca5a767f3cd3f7afb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-27 06:14:51 +0200

    Update ja_JP, thanks to Toramaru13

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'アルバムに適用する';
+$lang['Click here to see the picture'] = '画像を見るにはここをクリックしてください';
+$lang['Close'] = '閉じる';
+$lang['Invalid email address'] = '無効なメールアドレス';
+$lang['Photo shared'] = '画像が共有されました';
+$lang['Send email'] = 'メールを送信する';
+$lang['Share'] = '共有';
+$lang['Share link'] = 'リンクを共有する';
+$lang['Share this picture'] = 'この画像を共有する';
+$lang['a list of albums'] = 'アルバム一覧';
+$lang['all albums'] = '全てのアルバム';
