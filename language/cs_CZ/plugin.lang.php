e47472e98b394a48c1c5545d5392384e4210ed97
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-16 06:22:34 +0100

    Update cs_CZ, thanks to gandycz

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s has shared a photo with you'] = '%s s vámi sdílí fotku';
+$lang['Apply to albums'] = 'Aplikovat na alba';
+$lang['Click here to see the picture'] = 'Klikněte zde pro zobrazení obrázku';
+$lang['Close'] = 'Zavřít';
+$lang['Expires in'] = 'Expiruje za';
+$lang['Invalid email address'] = 'Neplatná e-mailová adresa';
+$lang['Photo shared'] = 'Fotografie nasdílena';
+$lang['Send email'] = 'Odeslat email';
+$lang['Share'] = 'Sdílet';
+$lang['Share link'] = 'Sdílet odkaz';
+$lang['Share this picture'] = 'Sdílet tento obrázek';
+$lang['Your email could not be sent'] = 'Váš e-mail se nepodařilo odeslat';
+$lang['Your email has been sent'] = 'Váš e-mail byl odeslán';
+$lang['a list of albums'] = 'seznam alb';
+$lang['all albums'] = 'všechna alba';
