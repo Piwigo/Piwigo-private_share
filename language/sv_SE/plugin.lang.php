d9cc22c8a25b931e967f398db068c7a19c2ef444
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-20 08:10:26 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s has shared a photo with you'] = '%s har delat ett foto med dig';
+$lang['Apply to albums'] = 'Applicera på album';
+$lang['Click here to see the picture'] = 'Klicka här för att se bilden';
+$lang['Close'] = 'Stäng';
+$lang['Expires in'] = 'Går ut om';
+$lang['Invalid email address'] = 'Ogiltig e-postadress';
+$lang['Photo shared'] = 'Foto delat';
+$lang['Send email'] = 'Skicka e-post';
+$lang['Share'] = 'Dela';
+$lang['Share link'] = 'Dela länk';
+$lang['Share this picture'] = 'Dela denna bild';
+$lang['Your email could not be sent'] = 'Ditt email kunde inte skickas';
+$lang['Your email has been sent'] = 'Ditt email har skickats';
+$lang['a list of albums'] = 'en lista av album';
+$lang['all albums'] = 'alla album';
