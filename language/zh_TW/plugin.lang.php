d8c6552a3928a5e2466d0d7f8c892838a8f6e07b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-23 09:43:06 +0100

    Update zh_TW, thanks to trekmorris

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = '分配至相簿';
+$lang['all albums'] = '所有相簿';
+$lang['a list of albums'] = '相簿清單';
+$lang['Your email has been sent'] = '您的信件已寄送';
+$lang['Your email could not be sent'] = '您的信件無法寄送';
+$lang['Share this picture'] = '分享此張照片';
+$lang['Share link'] = '分享連結';
+$lang['Share'] = '分享';
+$lang['Send email'] = '寄送Email';
+$lang['Photo shared'] = '照片已分享';
+$lang['Invalid email address'] = '無效的郵件地址';
+$lang['Expires in'] = '到期日';
+$lang['Close'] = '關閉';
+$lang['Click here to see the picture'] = '點擊此處查看照片';
+$lang['%s has shared a photo with you'] = '%s已經分享一張照片給您';
