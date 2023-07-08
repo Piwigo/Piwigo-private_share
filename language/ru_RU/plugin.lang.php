82f3fc024204982ee6472aed203a7a7cfa0d60d0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:28:46 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s has shared a photo with you'] = '%s поделился с вами фотографией';
+$lang['Apply to albums'] = 'Применить к альбомам';
+$lang['Click here to see the picture'] = 'Нажмите сюда для открытия фото';
+$lang['Close'] = 'Закрыть';
+$lang['Expires in'] = 'Истекает';
+$lang['Invalid email address'] = 'Неверный e-mail адрес';
+$lang['Photo shared'] = 'Фото открыто';
+$lang['Send email'] = 'Отправить письмо';
+$lang['Share'] = 'Поделиться';
+$lang['Share link'] = 'Поделиться ссылкой';
+$lang['Share this picture'] = 'Поделиться этим изображением';
+$lang['Your email could not be sent'] = 'Ваше письмо не может быть отправлено';
+$lang['Your email has been sent'] = 'Ваше письмо отправлено';
+$lang['a list of albums'] = 'список альбомов';
+$lang['all albums'] = 'все альбомы';
