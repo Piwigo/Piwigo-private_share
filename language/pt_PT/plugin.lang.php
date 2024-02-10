dd72c691c60058579fff38421cea52e1cc2c9f8c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-10 13:43:50 +0100

    Update pt_PT, thanks to ANO

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s has shared a photo with you'] = '%s compartilhou uma foto com você';
+$lang['Apply to albums'] = 'Aplicar a álbuns';
+$lang['Click here to see the picture'] = 'Clique aqui para ver a foto';
+$lang['Close'] = 'Fechar';
+$lang['Expires in'] = 'Termina em';
+$lang['Invalid email address'] = 'Inválido email';
+$lang['Photo shared'] = 'Foto partilhada';
+$lang['Send email'] = 'Enviar mail';
+$lang['Share'] = 'Partilhar';
+$lang['Share link'] = 'Partilhar link';
+$lang['Share this picture'] = 'Partilhar esta foto';
+$lang['Your email could not be sent'] = 'O seu email não pode ser enviado';
+$lang['Your email has been sent'] = 'O seu email foi enviado';
+$lang['a list of albums'] = 'uma lista de álbuns';
+$lang['all albums'] = 'Todos os álbuns';
