f3e43372f25856acec4ef324a6b264c8418ef7a1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:05:07 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s has shared a photo with you'] = '%s שיתף איתך תמונה';
+$lang['Apply to albums'] = 'להגיש בקשה לאלבומים';
+$lang['Click here to see the picture'] = 'לחץ כאן כדי לראות את התמונה';
+$lang['Close'] = 'סגור';
+$lang['Expires in'] = 'תפוג בתוקף ב';
+$lang['Invalid email address'] = 'כתובת דוא"ל לא תקפה';
+$lang['Photo shared'] = 'תמונה ששותפה';
+$lang['Send email'] = 'שלח דוא"ל';
+$lang['Share'] = 'שיתוף';
+$lang['Share link'] = 'שיתוף קישור';
+$lang['Share this picture'] = 'שיתוף של תמונה זו';
+$lang['Your email could not be sent'] = 'לא ניתן היה לשלוח את הדוא"ל שלך';
+$lang['Your email has been sent'] = 'הדוא"ל שלך נשלח';
+$lang['a list of albums'] = 'רשימת אלבומים';
+$lang['all albums'] = 'כל האלבומים';
