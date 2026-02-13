36bdd3999136dc6d2348561d3b64ed1cc3261799
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-13 06:50:51 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s has shared a photo with you'] = '%s(이)가 사진을 공유했습니다';
+$lang['Apply to albums'] = '앨범에 적용하기';
+$lang['Click here to see the picture'] = '사진 보러 가기';
+$lang['Close'] = '닫기';
+$lang['Expires in'] = '만료까지 남은 시간';
+$lang['Invalid email address'] = '잘못된 이메일 주소입니다';
+$lang['Photo shared'] = '사진 공유됨';
+$lang['Send email'] = '이메일 보내기';
+$lang['Share'] = '공유하기';
+$lang['Share link'] = '공유 링크';
+$lang['Share this picture'] = '이 사진 공유하기';
+$lang['Your email could not be sent'] = '이메일을 보낼 수 없습니다';
+$lang['Your email has been sent'] = '이메일이 전송되었습니다';
+$lang['a list of albums'] = '앨범 목록';
+$lang['all albums'] = '모든 앨범';
